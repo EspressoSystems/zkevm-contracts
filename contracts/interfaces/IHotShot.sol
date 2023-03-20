5dfc2da0f014637a05d2687609b0f649f4a99b04
Mathis Antony <sveitser@gmail.com>
2023-03-20 13:43:29 +0000

    Add NewBlocks event to IHotShot interface

--- contracts/interfaces/IHotShot.sol
+++ contracts/interfaces/IHotShot.sol
@@ -6,5 +6,7 @@ pragma solidity 0.8.17;
  * @dev Define interface for HotShot contract.
  */
 interface IHotShot {
+    event NewBlocks(uint256 firstBlockNumber, uint256 numBlocks);
+
     function commitments(uint256 blockNumber) external view returns (uint256);
 }
